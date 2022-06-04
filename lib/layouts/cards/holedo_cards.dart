246299a40449b78c932c6b2a8ec479db6afda781
John Ubalde <john@limabean.co.za>
2022-06-05 03:44:59 +0800

    ff

--- lib/layouts/cards/holedo_cards.dart
+++ lib/layouts/cards/holedo_cards.dart
@@ -2,12 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:holedo/models/holedoapi/job.dart';
-import 'package:holedo/models/holedoapi/article.dart';
-import 'package:holedo/models/holedoapi/article_category.dart';
+
 import 'package:routemaster/routemaster.dart';
 import 'package:intl/intl.dart';
 import 'package:holedo/models/holedoapi/user.dart';
 import 'package:holedo/models/models.dart';
+export 'package:holedo/layouts/cards/news_cards.dart';
 
 class CustomCard extends StatelessWidget {
   final Widget child;
@@ -102,62 +102,6 @@ class LinkCard extends StatelessWidget {
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class BookCard extends StatelessWidget {
-  final Book book;
-  final bool showReleaseDate;
-  final String Function(String id)? pathBuilder;
-
-  const BookCard({
-    Key? key,
-    required this.book,
-    this.showReleaseDate = false,
-    this.pathBuilder,
-  }) : super(key: key);
-
-  static final _formatter = DateFormat('yyyy-MM-dd');
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomCard(
-      onTap: () {
-        Routemaster.of(context).push(
-            pathBuilder != null ? pathBuilder!(book.id) : '/book/${book.id}');
-      },
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            height: 100,
-            width: 75,
-            child: Icon(
-              CupertinoIcons.book,
-              size: 55,
-              color: Colors.grey,
-            ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  book.title,
-                  style: TextStyle(fontSize: 16),
-                ),
-                if (showReleaseDate) Text(_formatter.format(book.releaseDate)),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
@@ -278,185 +222,6 @@ class UserCard extends StatelessWidget {
                 Text(data.dateOfBirth != null
                     ? _formatter.format(data.dateOfBirth as DateTime)
                     : '')
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class NewsCard extends StatelessWidget {
-  final Article article;
-  final bool showReleaseDate;
-  final String Function(String id)? pathBuilder;
-
-  const NewsCard({
-    Key? key,
-    required this.article,
-    this.showReleaseDate = false,
-    this.pathBuilder,
-  }) : super(key: key);
-
-  static final _formatter = DateFormat('yyyy-MM-dd');
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomCard(
-      onTap: () {
-        Routemaster.of(context).push(pathBuilder != null
-            ? pathBuilder!(article.slug as String)
-            : '/article/${article.articleCategories?.first.slug}/${article.slug}');
-      },
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(article.bannerImage.toString()),
-                fit: BoxFit.cover,
-                repeat: ImageRepeat.noRepeat,
-              ),
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(0),
-            ),
-            height: 100,
-            width: 75,
-            child: article.bannerImage != null
-                ? null
-                : Icon(
-                    CupertinoIcons.news,
-                    size: 55,
-                    color: Colors.grey,
-                  ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  article.title as String,
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                Text('------------------'),
-                for (final cat in article.articleCategories!)
-                  Text(
-                    cat.title as String,
-                    style: TextStyle(fontSize: 12),
-                  ),
-                if (showReleaseDate)
-                  Text(_formatter.format(article.created as DateTime)),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class NewsCategoryCard extends StatelessWidget {
-  final ArticleCategory category;
-  final bool showReleaseDate;
-  final String Function(String id)? pathBuilder;
-
-  const NewsCategoryCard({
-    Key? key,
-    required this.category,
-    this.showReleaseDate = false,
-    this.pathBuilder,
-  }) : super(key: key);
-
-  static final _formatter = DateFormat('yyyy-MM-dd');
-
-  @override
-  Widget build(BuildContext context) {
-    return SmallCard(
-      onTap: () {
-        Routemaster.of(context).push(pathBuilder != null
-            ? pathBuilder!(category.slug as String)
-            : '/news/all/${category.slug}/');
-      },
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 0, 0, 0),
-              borderRadius: BorderRadius.circular(3),
-            ),
-            height: 30,
-            width: 25,
-            child: Icon(
-              CupertinoIcons.news,
-              size: 15,
-              color: Colors.grey,
-            ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  category.title as String,
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FooterCard extends StatelessWidget {
-  final String Function(String id)? pathBuilder;
-
-  const FooterCard({
-    Key? key,
-    this.pathBuilder,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SmallCard(
-      onTap: () {
-        Routemaster.of(context).push(pathBuilder != null
-            ? pathBuilder!('aa' as String)
-            : '/news/all/aa/');
-      },
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 0, 0, 0),
-              borderRadius: BorderRadius.circular(3),
-            ),
-            height: 30,
-            width: 25,
-            child: Icon(
-              CupertinoIcons.news,
-              size: 15,
-              color: Colors.grey,
-            ),
-          ),
-          SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'aaa',
-                  style: TextStyle(fontSize: 16),
-                ),
               ],
             ),
           ),
